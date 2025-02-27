7a75b731065fd8d199dcba88bf999bae8169bc0e
harryitc <cuongharryit@gmail.com>
2025-02-27 01:24:42 +0000

    Refactor: remove console log

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,8 +23,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log('pathname', pathname);
-  console.log('links', links);
   return (
     <>
       {links.map((link) => {
